df5c976edce30476c6f8c14232173c9162a06bc1
scar1377 <scarlettadams77@gmail.com>
2021-11-04 11:18:23 +0000

    updates package script && updates connection.js

--- listen.js
+++ listen.js
@@ -1,5 +1,7 @@
 const app = require("./app.js");
 
+const { PORT = 9090 } = process.env;
+
 app.listen(9090, () => {
-  console.log("listening for port 9090...");
+  console.log(`listening for szanews on ${PORT}...`);
 });
