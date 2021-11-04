d0b81242e3e2c4c69e1f4fcc27ced70c3807b4b6
scar1377 <scarlettadams77@gmail.com>
2021-11-04 11:52:48 +0000

    updates listen.js

--- listen.js
+++ listen.js
@@ -2,6 +2,7 @@ const app = require("./app.js");
 
 const { PORT = 9090 } = process.env;
 
-app.listen(9090, () => {
-  console.log(`listening for szanews on ${PORT}...`);
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Listening on ${PORT}...`);
 });
