3a73e237e96714de138fbda594f26be6d3c91178
scar1377 <scarlettadams77@gmail.com>
2021-11-04 11:29:31 +0000

    runs seed:prod

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,9 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE not set or DATABASE_URL not set");
+}
 const config =
   ENV === "production"
     ? {
@@ -14,9 +17,5 @@ const config =
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE not set or DATABASE_URL not set");
-}
 
 module.exports = new Pool(config);
